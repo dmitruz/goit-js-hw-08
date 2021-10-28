9b261d09b7f39c3cffffaadd8d76bf8e8ee4d4e0
dmitruz <dmitruz970@gmail.com>
2021-10-28 21:40:05 +0300

    1 quiz

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,35 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryImages = document.querySelector('.gallery');
+const cardsImage = createGalleryItem(galleryItems);
+
+galleryImages.insertAdjacentHTML('beforeend', cardsImage);
+
+function createGalleryItem(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+            return `
+            <a class="gallery__item" href="${original}">
+             <img class="gallery__image"
+             src="${preview}"
+             alt="${description}" />
+            </a>
+        `;
+        })
+        .join('');
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+    
+ });
+
 
 console.log(galleryItems);
-console.log('Hello');
+
