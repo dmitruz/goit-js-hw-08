4f4355d9a42ce523505db4efb40ce09b033d8fa2
dmitruz <dmitruz970@gmail.com>
2021-10-30 18:16:41 +0300

    внес правки

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,36 +1,53 @@
-
-
 import SimpleLightbox from 'simplelightbox';
+import { galleryItems } from './gallery-items.js';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-import { galleryItems } from './gallery-items';
+
+
+
 
 const galleryImages = document.querySelector('.gallery');
 const cardsImage = createGalleryItem(galleryItems);
+let originalImage;
 
 galleryImages.insertAdjacentHTML('beforeend', cardsImage);
+
+galleryImages.addEventListener('keydown', onClickEscCloseImage);
 
 function createGalleryItem(galleryItems) {
     return galleryItems
         .map(({ preview, original, description }) => {
             return `
-            <a class="gallery__item" href="${original}">
-             <img class="gallery__image"
-             src="${preview}"
-             alt="${description}" />
-            </a>
+        <div class="gallery__item">
+            <a class="gallery__link" href="large-image.jpg">
+                <img
+                  class="gallery__image"
+                  src="${preview}"
+                  data-source="${original}"
+                  alt="${description}"
+                 />
+             </a>
+        </div>
         `;
         })
         .join('');
 }
 
-const lightbox = new SimpleLightbox('.gallery a', {
-    captions: true,
-    captionsData: 'alt',
-    captionPosition: 'bottom',
-    captionDelay: 250,
-    
- });
+galleryImages.addEventListener('click', evt => {
+    evt.preventDefault();
+    if (evt.target.nodeName !== "IMG") {
+        return;
+    }
+    originalImage = basicLightbox.create(`
+		<img src="${evt.target.dataset.source}" width="1200" height="900">
+	`)
+    originalImage.show()
+});
 
 
+function onClickEscCloseImage(evt) {
+if (evt.code !== 'Escape') {
+      return;
+    }
+    originalImage.close();
+}
 console.log(galleryItems);
-
