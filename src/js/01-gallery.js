0983218ed0ad1d80daf2548fcfc5353c75ab69a2
dmitruz <dmitruz970@gmail.com>
2021-10-30 14:42:04 +0300

    deleated parcel new version

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,5 @@
 
-// Change code below this line
+
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
